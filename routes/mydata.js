4eed9d53957b7afa1c184a799294a9c6288d8ecf
Manikanta Nelluri <nellurimanikantabtech@gmail.com>
2023-10-24 20:31:40 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Nelluri Naga Manikanta' });
+  res.render('mydata', { title: 'Naga Manikanta Nelluri' });
 });
 module.exports = router;
